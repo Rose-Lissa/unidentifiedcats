0a33d85efc4e2180f48f62c115ffd2c2bdafd148
Dmitry <d.rodionov1@g.nsu.ru>
2023-05-12 21:51:49 +0700

    task add message and message Api done

--- src/main/java/ru/nsu/fit/chat/config/SecurityConfiguration.java
+++ src/main/java/ru/nsu/fit/chat/config/SecurityConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.nsu.fit.chat.Service.UserService;
+import ru.nsu.fit.chat.service.UserService;
 
 @Configuration
 @EnableWebSecurity
